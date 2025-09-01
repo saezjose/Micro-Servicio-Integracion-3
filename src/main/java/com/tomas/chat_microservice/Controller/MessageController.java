0bb5553374a4780e90b90ca11426ea254606ea91
deimon005 <134963531+deimon005@users.noreply.github.com>
2025-09-01 00:00:34 -0400

    pruebas swagger y postman hechos

--- src/main/java/com/tomas/chat_microservice/Controller/MessageController.java
+++ src/main/java/com/tomas/chat_microservice/Controller/MessageController.java
@@ -1,13 +1,15 @@
 package com.tomas.chat_microservice.Controller;
-
 import com.tomas.chat_microservice.Model.Message;
 import com.tomas.chat_microservice.Service.MessageService;
-import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/messages")
+@Tag(name = "Mensajes", description = "Operaciones para enviar y recibir mensajes")
 public class MessageController {
 
     private final MessageService messageService;
@@ -17,11 +19,15 @@ public class MessageController {
     }
 
     @GetMapping("/{receiver}")
+    @Operation(summary = "Obtener mensajes por receptor",
+            description = "Devuelve todos los mensajes enviados a un usuario en específico.")
     public List<Message> getMessages(@PathVariable String receiver) {
         return messageService.getMessagesByUser(receiver);
     }
 
     @PostMapping
+    @Operation(summary = "Enviar un mensaje",
+            description = "Permite enviar un nuevo mensaje indicando emisor, receptor y contenido.")
     public void sendMessage(@RequestBody Message message) {
         messageService.sendMessage(message);
     }
