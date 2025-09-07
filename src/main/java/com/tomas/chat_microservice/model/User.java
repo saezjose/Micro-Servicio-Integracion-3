def6cffbd23e1e7e42c825d984efa7a994e963a8
cchelooo <msantanaastorga25@gmail.com>
2025-09-07 19:30:40 -0300

    Tareas II

---
+++ src/main/java/com/tomas/chat_microservice/model/User.java
@@ -0,0 +1,30 @@
+package com.tomas.chat_microservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    private Instant createdAt;
+}
