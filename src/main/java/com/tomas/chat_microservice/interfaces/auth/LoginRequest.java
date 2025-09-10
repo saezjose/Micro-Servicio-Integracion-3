f9b8be72a02193e3017c2daa45a0dc1638ec6da4
saezjose <134319766+saezjose@users.noreply.github.com>
2025-09-09 22:25:05 -0300

    DTO LoginRequest {email,password}

--- src/main/java/com/tomas/chat_microservice/interfaces/auth/LoginRequest.java
+++ src/main/java/com/tomas/chat_microservice/interfaces/auth/LoginRequest.java
@@ -8,4 +8,7 @@ public class LoginRequest {
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+
 }
+
